8a8719a6d325c626520a7991577a7c9a04a5f569
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-10-23 15:06:29 +0530

    This is my second script

--- Variables.sh
+++ Variables.sh
@@ -7,3 +7,7 @@ echo "$person1: Hello $person2, Good Morning"
 echo "$person2: Hi $person1,Very Good Morning"
 echo "$person1: How are you $person2?"
 echo "$person2: I am good $person1, How are you"
+
+
+date=$(date)
+echo "Date and time is: ${Date}"
