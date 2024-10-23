a36adbbbeacd21a2e312262e5f0a2ab55274d40a
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-10-23 22:56:51 +0530

    This is my second script

--- install_my_sql.sh
+++ install_my_sql.sh
@@ -17,3 +17,11 @@ then
 else
      echo "Installing Mysql is Success"
 fi
+yum install git -y
+if [$? -ne 0 ]
+then
+    echo "Error:: Installing git is failed"
+    exit 1
+else
+    echo "Installing git is success"
+fi
