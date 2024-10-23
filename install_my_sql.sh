c1856e2eaf26936f49de25aded30c14cf7ed0ae1
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-10-23 22:43:36 +0530

    This is my second script

--- install_my_sql.sh
+++ install_my_sql.sh
@@ -10,7 +10,7 @@ else
 fi #fi means reverse of if ,indicating condition end
 yum install mysqllll -y
 
-if [$? -ne 0 ]
+if [ $? -ne 0 ]
 then
     echo "Error:: Installing Mysql is failed"
     exit 1
