7b3abe7774e8ce729189b35e6ba087948ebacf97
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-10-23 15:48:31 +0530

    This is my second script

--- Variables2.sh
+++ Variables2.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-Username=$1
-Password=$2
+# Username=$1
+# Password=$2
 
-echo "User_name is: $Username and Password is: $Password"
+# echo "User_name is: $Username and Password is: $Password"
+
+# In this code username and password will be displayed we will write cod ewithout displaying it
+
+echo "Please enter Username"
+read Username #the value entered above will be automatically attached to username variable
+echo "Username is: $Username"
