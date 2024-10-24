fc61ce7eefc4d8b61a6c0300f99b7321d2174de3
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-10-24 23:47:05 +0530

    pushing the code

--- 11.Insatll_packages.sh
+++ 11.Insatll_packages.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
 Id=$(id -u)
+timestamp=$(date +%F-%H-%M-%S)
+Logs="/tmp/$0-$timestamp.log"
+R=\e[31m
+G=\e[32m
+Y=\e[33m
+N=\e[0m
+echo "Script started excuting $timestamp" &>> $Logs
 validate(){
     if [ $1 -ne 0 ]
 then
-    echo "Error::$2 ...Failed"
+    echo -e "Error::$2 ... $R Failed $N"
     exit 1
 else
-    echo "$2 ..Success"
+    echo -e "$2 ..$G Success $N"
 fi
 
 }
 if [ $Id -ne 0 ]
 then 
-    echo -e "Error :: please run this script in root access"
+    echo -e "$R Error :: please run this script in root access $N"
     exit 1
 else
     echo -e "You are root user"
 fi
-yum install mysql -y
+yum install mysql -y &>> $Logs
 validate $? "Installing mysql"
-yum install git -y
+yum install git -y &>> $Logs
 validate $? "Installing git"
