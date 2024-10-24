90cf32285e782ed3dd16f9f0c03698735eaebf37
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-10-24 23:27:02 +0530

    pushing the code

--- 11.Insatll_packages.sh
+++ 11.Insatll_packages.sh
@@ -3,8 +3,15 @@ Id=$(id -u)
 if [ $Id -ne 0 ]
 then 
     echo -e "Error :: please run this script in root access"
-    # exit 1
+    exit 1
 else
     echo -e "You are root user"
 fi
-yum install mysqllll -y
+yum install mysql -y
+if [$? -ne 0]
+then
+    echo "Error:: mysql ...Failed"
+    exit 1
+else
+    echo "mysql ..Success"
+fi
