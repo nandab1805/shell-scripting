0d0bef36b380fd809c45a091b7d83bb90ae3e613
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-10-24 19:30:01 +0530

    pushing the code

--- Logs.sh
+++ Logs.sh
@@ -2,18 +2,24 @@
 Id=$(id -u)
 timestamp=$(date +%F-%H-%M-%S)
 Logfile="/tmp/$0-$timestamp.log"
+R="\e[31m"
+Y="\e[32m"
+G="\e[33m"
+N="\e[0m"
+
+
 validate(){
     if [ $1 -ne 0 ]
     then
-        echo "Error:: $2 ... failed"
+        echo "Error:: $2 ... $R failed $N"
         exit 1
     else
-        echo "$2 ...Success"
+        echo "$2 ...$G Success $N"
     fi
 }
 if [ $Id -ne 0 ]
 then
-    echo "Error::Please run this script with root access"
+    echo "$R Error::Please run this script with root access $N"
     exit 1 
 else
     echo "You are Root User"
