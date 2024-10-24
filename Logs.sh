854dec75ab2b56c76b4803c630d3703fa31e77a8
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-10-24 14:06:30 +0530

    pushing the code

---
+++ Logs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+Id=$(id -u)
+echo "Script name : $0"
+validate(){
+    if [ $1 -ne 0 ]
+    then
+        echo "Error:: $2 ... failed"
+        exit 1
+    else
+        echo "$2 ...Success"
+    fi
+}
+if [ $Id -ne 0 ]
+then
+    echo "Error::Please run this script with root access"
+    exit 1 
+else
+    echo "You are Root User"
+fi
+
+yum install mysql -y
+validate $? "Installed mysql"
+yum install git -y
+validate $? "Installed git"
